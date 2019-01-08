4989357a8a623441ee2b74576a0d71066ed21648
Elizabeth Cottrell <elizabeth@openforge.io>
2019-01-08 18:38:48 -0500

    Updates

---
+++ precache-manifest.c10bfc59f9f5dec76433c19e1a4fe705.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/elizacottrell-v3/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "49ce5e4c1154d719492d",
+    "url": "/elizacottrell-v3/static/js/runtime~main.49ce5e4c.js"
+  },
+  {
+    "revision": "2cf35458a8773976824e",
+    "url": "/elizacottrell-v3/static/js/main.2cf35458.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/elizacottrell-v3/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "2cf35458a8773976824e",
+    "url": "/elizacottrell-v3/static/css/main.fbe25534.chunk.css"
+  },
+  {
+    "revision": "bda605eb62de57e00c80b4130fbf669e",
+    "url": "/elizacottrell-v3/index.html"
+  }
+];
