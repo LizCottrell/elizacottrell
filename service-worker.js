d148e654c1387944f5684d1021feea960ba26b43
Elizabeth Cottrell <elizabeth@openforge.io>
2019-01-10 19:16:34 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/elizacottrell-v3/precache-manifest.9b9c0a5e50cc2c44d304a8485829a6cc.js"
+  "/elizacottrell-v3/precache-manifest.18f61245e32d4d8525464eee4f9e5242.js"
 );
 
 workbox.clientsClaim();
