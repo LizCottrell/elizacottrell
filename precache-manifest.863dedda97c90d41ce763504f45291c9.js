dbd5acd5ee2dbe187ed684b618e80b22b09e2553
Elizabeth Cottrell <elizabeth@openforge.io>
2019-01-10 23:22:52 -0500

    Updates

---
+++ precache-manifest.863dedda97c90d41ce763504f45291c9.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "01d93121833ee78184c62ba346577ed2",
+    "url": "/static/media/farmer.01d93121.png"
+  },
+  {
+    "revision": "00e30b8511eee6522678",
+    "url": "/static/css/main.819d2270.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "85b9ec6e06d34971a0a2",
+    "url": "/static/js/1.85b9ec6e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0d487c26c3ca015ac257e99e3ad59a24",
+    "url": "/static/media/batala.0d487c26.png"
+  },
+  {
+    "revision": "73417b097693dcedbe3a7ec2d20d6148",
+    "url": "/static/media/hoagiefest.73417b09.png"
+  },
+  {
+    "revision": "00e30b8511eee6522678",
+    "url": "/static/js/main.00e30b85.chunk.js"
+  },
+  {
+    "revision": "b2cd97e870438772c031fad7e7f70314",
+    "url": "/static/media/archaeology.b2cd97e8.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "85b9ec6e06d34971a0a2",
+    "url": "/static/css/1.78e5d7e1.chunk.css"
+  },
+  {
+    "revision": "2b2d44393b475c64e54191c285ea284a",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/elizacottrell-v3/precache-manifest.283d46dccec47223efbff258af3b90b8.js"
+  "/precache-manifest.863dedda97c90d41ce763504f45291c9.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/elizacottrell-v3/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
